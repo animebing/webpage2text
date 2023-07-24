369d1da436936325e59e12acd09f7a9801fa25f2
Yanbing Dong <dongyanbing@metasota.ai>
2023-07-24 12:51:17 +0800

    js

---
+++ static/app.js
@@ -0,0 +1,33 @@
+function extractText() {
+    const url = document.getElementById('url').value;
+    if (url) {
+      fetch('/api/extract?url=' + url)
+        .then(response => response.text())
+        .then(data => {
+          const text = JSON.parse(data)
+          console.log('bingbing: ' + text);
+          document.getElementById('text').value = text;
+        })
+        .catch(error => console.log(error));
+    }
+  }
+
+function copyText() {
+  const text = document.getElementById('text');
+  text.select();
+  document.execCommand('copy');
+}
+
+function saveText() {
+  const text = document.getElementById('text').value;
+  const filename = 'text.txt';
+  const blob = new Blob([text], {type: 'text/plain'});
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.download = filename;
+  link.href = url;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
