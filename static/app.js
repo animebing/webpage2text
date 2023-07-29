ab0cca41304f8372beed53420f5a37d3ca93670b
animebing <yanbing_dong@hotmail.com>
2023-07-29 21:34:45 +0800

    corner case

--- static/app.js
+++ static/app.js
@@ -1,29 +1,61 @@
 function extractText() {
     const url = document.getElementById('url').value;
-    if (url) {
-      fetch('/api/extract?url=' + url)
-        .then(response => response.text())
-        .then(data => {
-          const text = JSON.parse(data);
-          document.getElementById('text').value = text;
-        })
-        .catch(error => console.log(error));
+    if (url === "") {
+      alert("Please enter a url");
+      return;
     }
-  }
+
+    const button_0 = document.getElementById('button_0');
+    button_0.innerHTML = '<span class="loading"><span class="inner"></span><span class="inner"></span><span class="inner"></span></span>'
+    
+    const button_1 = document.getElementById('button_1');
+    const button_2 = document.getElementById('button_2');
+    button_1.disabled = true;
+    button_2.disabled = true;
+
+    document.getElementById('text').value = "";
+    fetch('/api/extract?url=' + url)
+      .then(response => response.text())
+      .then(data => {
+        const text = JSON.parse(data);
+        if (text === null) {
+          alert("Extraction fails");
+          return;
+        }
+        document.getElementById('text').value = text;
+      })
+      .catch(error => console.log(error))
+      .finally(() => {
+        button_0.innerHTML = "Extract Text"
+        button_1.disabled = false;
+        button_2.disabled = false;
+      });
+}
 
 function copyText() {
   const text = document.getElementById('text').value;
+  if (text === "") {
+    alert("There is no text");
+    return;
+  }
+
   navigator.clipboard.writeText(text)
     .then(() => {
-      alert("Copied text to keyboard");
+      alert("Copied to clipboard");
     })
     .catch((err) => {
       console.error('Error copying text: ', err);
     });
+  
 }
 
 function saveText() {
   const text = document.getElementById('text').value;
+  if (text === "") {
+    alert("There is no text");
+    return;
+  }
+
   const filename = 'text.txt';
   const blob = new Blob([text], {type: 'text/plain'});
   const url = URL.createObjectURL(blob);
