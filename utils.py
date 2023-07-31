b2ece55353f619dec352957ee058d6ccd42e6564
animebing <yanbing_dong@hotmail.com>
2023-07-31 23:02:32 +0800

    use inscriptis to replace trafilatura

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
-from trafilatura import fetch_url, extract
+import re
+import urllib.request
+
+from inscriptis import get_text
 
 def do_extract(url: str):
-    downloaded = fetch_url(url)
-    text = extract(downloaded)
+    html = urllib.request.urlopen(url).read().decode('utf-8')
+    text = get_text(html)
+    print(text)
+    text = re.sub('\n+', '\n', text)
+    text = re.sub(' +', ' ', text)
 
     return text
